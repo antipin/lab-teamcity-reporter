c681daa1915f7e4be7e560a540ca3a8098f3d166
Alexander Antipin <alex@antipin.com>
2015-08-05 23:09:55 +0300

    Reporter.prototype.log replaced with teamcity-service-messages module

--- src/teamcity.js
+++ src/teamcity.js
@@ -1,4 +1,5 @@
-var SUITE_TITLE_DELIMITER = ' -> ',
+var tsm = require('teamcity-service-messages'),
+    SUITE_TITLE_DELIMITER = ' -> ',
     internals = {};
 
 /**
@@ -32,18 +33,18 @@ internals.Reporter.prototype.test = function (test) {
 
         if (this.previousSuiteTitle) {
 
-            this.log('testSuiteFinished', { name: this.previousSuiteTitle });
+            tsm.testSuiteFinished({ name: this.previousSuiteTitle });
         }
 
         this.previousSuiteTitle = suiteTitle;
 
-        this.log('testSuiteStarted', { name: suiteTitle });
+        tsm.testSuiteStarted({ name: suiteTitle });
     }
 
-    this.log('testStarted', { name: test.relativeTitle, captureStandardOutput: true });
+    tsm.testStarted({ name: test.relativeTitle, captureStandardOutput: true });
 
     if (test.err) {
-        this.log('testFailed', {
+        tsm.testFailed({
             name: test.relativeTitle,
             messge: test.err.message,
             details: test.err.stack,
@@ -55,11 +56,11 @@ internals.Reporter.prototype.test = function (test) {
 
         skippedMessage = test.todo ? 'no callback provided' : 'test skipped';
 
-        this.log('testIgnored', { name: test.relativeTitle, message: skippedMessage });
+        tsm.testIgnored({ name: test.relativeTitle, message: skippedMessage });
 
     } else {
 
-        this.log('testFinished', { name: test.relativeTitle, duration: test.duration });
+        tsm.testFinished({ name: test.relativeTitle, duration: test.duration });
     }
 };
 
@@ -71,41 +72,6 @@ internals.Reporter.prototype.end = function (notebook) {
 
     if (this.previousSuiteTitle) {
 
-        this.log('testSuiteFinished', { name: this.previousSuiteTitle });
+        tsm.testSuiteFinished({ name: this.previousSuiteTitle });
     }
-};
-
-/**
- * Logs string in a special format acceptable by teamcity
- * See [TeamCity reporting tests]{@link https://confluence.jetbrains.com/display/TCD65/Build+Script+Interaction+with+TeamCity#BuildScriptInteractionwithTeamCity-ReportingTests}
- * @param {string} title
- * @param {Object} params
- */
-internals.Reporter.prototype.log = function(title, params) {
-
-    var attrs = Object.keys(params).map(function(paramName) {
-        return [ paramName, "'" + this.escapeString(params[paramName]) + "'" ].join('=');
-    }, this);
-
-    console.log("##teamcity[" + title + " " + attrs.join(' ') + "]");
-};
-
-/**
- * @param {string} value
- * @returns {string} Escaped string
- */
-internals.Reporter.prototype.escapeString = function(value) {
-
-    if (!value) return '';
-
-    return value.toString()
-        .replace(/\|/g, "||")
-        .replace(/\n/g, "|n")
-        .replace(/\r/g, "|r")
-        .replace(/\[/g, "|[")
-        .replace(/\]/g, "|]")
-        .replace(/\u0085/g, "|x")
-        .replace(/\u2028/g, "|l")
-        .replace(/\u2029/g, "|p")
-        .replace(/'/g, "|'");
 };
